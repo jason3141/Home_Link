947d54f04d24df93ebe71adfd213b7cb60074340
jyjsjd <jyjsjd@hotmail.com>
2016-06-03 20:07:41 +0800

    hide something
    hide something

--- Home_Link/middlewares.py
+++ Home_Link/middlewares.py
@@ -5,7 +5,7 @@ import base64
 
 class ProxyMiddleware(object): 
 	def process_request(self, request, spider):
-		request.meta['proxy'] = 'http://naannawsg01-v.asce.corp.bshg.com:8082'
-		proxy_user_pass = "CZH\NAA-RGC-EDIX:(ijn0okm"
+		request.meta['proxy'] = 'http://proxy.com:port'
+		proxy_user_pass = "username:password"
 		encoded_user_pass = base64.encodestring(proxy_user_pass)
 		request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
