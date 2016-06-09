c36d1f4a06a8144057373001c3492639d9ea8994
jyjsjd <jyjsjd@hotmail.com>
2016-06-09 08:11:36 +0800

    No change, PEP8.

--- Home_Link/middlewares.py
+++ Home_Link/middlewares.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import base64 
+import base64
 
 
-class ProxyMiddleware(object): 
-	def process_request(self, request, spider):
-		request.meta['proxy'] = 'http://proxy.com:port'
-		proxy_user_pass = "username:password"
-		encoded_user_pass = base64.encodestring(proxy_user_pass)
-		request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
+class ProxyMiddleware(object):
+    def process_request(self, request, spider):
+        request.meta['proxy'] = 'http://proxy.com:port'
+        proxy_user_pass = "username:password"
+        encoded_user_pass = base64.encodestring(proxy_user_pass)
+        request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
